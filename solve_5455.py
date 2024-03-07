4ba04b859e1f9ed374685d1bb65a32b0db721b43
mortlach <mortlachmortlach@gmail.com>
2024-03-07 17:34:37 +0000

    prime_run

--- solve_5455.py
+++ solve_5455.py
@@ -59,14 +59,18 @@ def apply_key(decrypt_data, outputfile):
                         # a hit
                         this_pt_as_string = kd.get_as_runeglish_plaintext(this_pt=rot_pt, this_wli=wli_at_this_index)
                         if this_pt_as_string not in hit_dict:
-                            datatosave = [this_pt_as_string, this_i, result1, sum(result1), opts_for_this_pt]
-                            hit_dict[this_pt_as_string] = datatosave
-                            hit_data.append(datatosave)
-                            print(f'hit {datatosave}\n')
-                            outputfile.write(f'{datatosave}')
-                            outputfile.flush()
+                            datatosave = [decrypt_data['key_guess'], this_pt_as_string, this_i, result1, sum(result1), opts_for_this_pt]
+                            if datatosave not in hit_data:
+                                hit_data.append(datatosave)
+                                print(f'hit {datatosave}')
+                                outputfile.write(f'{datatosave}\n')
+                                outputfile.flush()
+                            if this_pt_as_string not in hit_dict:
+                                hit_dict[this_pt_as_string] = [datatosave]
+                            else:
+                                hit_dict[this_pt_as_string].append(datatosave)
 
-    return sorted(hit_data, key=itemgetter(3), reverse=True), total_attempts, tru_hits
+    return sorted(hit_data, key=itemgetter(4), reverse=True), total_attempts, tru_hits, hit_dict
 
 
 decrypt_data = {}
@@ -86,27 +90,59 @@ decrypt_data['wli'] = []
 [[decrypt_data['wli'].append([i, len(word)]) for i, x in enumerate(word)] for word in
  decrypt_data['c_runewords']]
 
-decrypt_data['key_guess'] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+decrypt_data['key_guesses'] = [
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71],
+    [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597],
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597],
+    [3301, 3299, 3271, 3257, 3229, 3191, 3119, 3001, 2819, 2579, 2131, 1459, 457],
+]
 
-decrypt_data["dec_function"] = [cry.decrypt_p_xor_k_to_p,
-                                cry.decrypt_k_xor_p_to_p,
-                                cry.decrypt_p_plus_k_to_p,
-                                cry.decrypt_p_minus_k_to_p,
-                                cry.decrypt_k_minus_p_to_p,
-                                cry.decrypt_p_multiply_k_to_p,
-                                cry.decrypt_p_divide_k_to_p,
-                                cry.decrypt_k_divide_p_to_p]
+import key_list
+decrypt_data['key_guesses'] = key_list.key_list
+
+decrypt_data["dec_function"] = [#cry.decrypt_p_xor_k_to_p,
+                                #cry.decrypt_k_xor_p_to_p,
+                                cry.decrypt_p_plus_k_to_p
+                                #cry.decrypt_p_minus_k_to_p,
+                                #cry.decrypt_k_minus_p_to_p,
+                                #cry.decrypt_p_multiply_k_to_p,
+                                #cry.decrypt_p_divide_k_to_p,
+                                #cry.decrypt_k_divide_p_to_p
+                                ]
 decrypt_data['interrupters'] = [None, 0, 5, 6, 7, 8, 11, 12, 13, 14, 17, 19, 20, 21, 22, 23, 25, 26, 27, 28]
 decrypt_data['transpositions'] = ['L2R', 'R2L']
 
+
+ahits = []
 with open('./test_5455.txt', 'w') as f:
-    for method in decrypt_data["dec_function"]:
-        print(f'STARTING METHOD {method}')
-        apply_key_start = time.time()
-        decrypt_data['decrypt_functions'] = [method]
-        print(cry.get_gematria_options_for_method(decrypt_data["decrypt_functions"][0]))
-        decrypt_data['c_and_k_directions'], decrypt_data['c_and_k_rotations'] = cry.get_gematria_options_for_method(
-            decrypt_data["decrypt_functions"][0])
-        hits, total_attempts, tru_hits = apply_key(decrypt_data, f)
-        print(
-            f'This iteration found {tru_hits} / {len(hits)}  / {total_attempts} Attempts {time.time() - apply_key_start} secs ')
+    for key_guess in decrypt_data["key_guesses"]:
+        decrypt_data['key_guess'] = key_guess
+        for method in decrypt_data["dec_function"]:
+            print(f'STARTING METHOD {method}')
+            apply_key_start = time.time()
+            decrypt_data['decrypt_functions'] = [method]
+            #print(cry.get_gematria_options_for_method(decrypt_data["decrypt_functions"][0]))
+            decrypt_data['c_and_k_directions'], decrypt_data['c_and_k_rotations'] = cry.get_gematria_options_for_method(
+                decrypt_data["decrypt_functions"][0])
+            hits, total_attempts, tru_hits, hit_dict = apply_key(decrypt_data, f)
+            ahits.extend(hits)
+            print(
+                f'This iteration found {tru_hits} / {len(hits)}  / {total_attempts} Attempts {time.time() - apply_key_start} secs ')
+            with open('./sorted_test_5455.txt', 'w') as f2:
+                for hit in hits:
+                    f2.write(f'{hit}\n')
+                    f2.flush()
+
+unique_hits= []
+for i in ahits:
+    if i in unique_hits:
+        pass
+    else:
+        unique_hits.append(i)
+
+
+with open('allsorted_test_5455_LPkeys.txt', 'w') as f2:
+    for hit in sorted(unique_hits, key=itemgetter(4), reverse=True):
+        f2.write(f'{hit}\n')
+        f2.flush()
+
